1ce1911dc0cb37d6152cf29d75e2c55919983e20
invincible <gabrielmbaiorga@gmail.com>
2023-12-23 14:37:30 +0100

    Create git project

---
+++ src/main/java/org/sdl/mintyn_bank_test/MintynBankTestApplication.java
@@ -0,0 +1,13 @@
+package org.sdl.mintyn_bank_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintynBankTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MintynBankTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sdl/mintyn_bank_test/MintynBankTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sdl.mintyn_bank_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MintynBankTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
